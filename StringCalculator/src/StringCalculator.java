6b3f2c538ea90cbcce60ffd18aaadfb365fb63ed
sanket <sanketwani4@gmail.com>
2021-10-16 18:34:01 +0530

    fourth commit

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -2,9 +2,10 @@ public class StringCalculator {
 	public static int add(final String numbers) {
 	    int returnValue = 0;
 	    String[] numbersArray = numbers.split(",");
-	    if (numbersArray.length > 2) {
-	        throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-	    }
+	    // Removed after exception
+	    // if (numbersArray.length > 2) {
+	    // throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+	    // }
 	    for (String number : numbersArray) {
 	        if (!number.trim().isEmpty()) { // After refactoring
 	            returnValue += Integer.parseInt(number);
--- StringCalculator/src/StringCalculatorTest.java
+++ StringCalculator/src/StringCalculatorTest.java
@@ -1,13 +1,13 @@
 		import org.junit.Test;
 
-import junit.framework.Assert;
+		import junit.framework.Assert;
 		
 		 
 		public class StringCalculatorTest {
-		    @Test(expected = RuntimeException.class)
+		    /*@Test(expected = RuntimeException.class)
 		    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
 		        StringCalculator.add("1,2,3");
-		    }
+		    }*/
 		    @Test
 		    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
 		        StringCalculator.add("1,2");
@@ -33,6 +33,14 @@ import junit.framework.Assert;
 				public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
 					Assert.assertEquals(3+6, StringCalculator.add("3,6"));
 			}
-		
+			
+		//  @Test(expected = RuntimeException.class)
+		//  public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+		//	StringCalculator.add("1,2,3");
+		//  }
+		    @Test
+		    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
+		        Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
+		    }
 	
 			}
